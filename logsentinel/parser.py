46cbdf022f7cdeade077a3d9dc390a849548a34a
Inarie351 <kim.degnon@epitech.eu>
2026-09-17 17:40:13 +0100

    [Feat]: add regexes to catch failed, invalid and successful logins

--- logsentinel/parser.py
+++ logsentinel/parser.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import Optional
 
@@ -9,3 +10,27 @@ class SSHEvent:
     user: str
     ip: str
     port: Optional[str] = None
+
+PATTERNS = [
+    (
+        re.compile(
+            r"^(?P<ts>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*sshd.*:\s+"
+            r"Failed password for (?P<user>\S+) from (?P<ip>[\d.:a-fA-F]+) port (?P<port>\d+)"
+        ),
+        "failed",
+    ),
+    (
+        re.compile(
+            r"^(?P<ts>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*sshd.*:\s+"
+            r"Failed password for invalid user (?P<user>\S+) from (?P<ip>[\d.:a-fA-F]+) port (?P<port>\d+)"
+        ),
+        "invalid_user",
+    ),
+    (
+        re.compile(
+            r"^(?P<ts>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*sshd.*:\s+"
+            r"Accepted (?:password|publickey) for (?P<user>\S+) from (?P<ip>[\d.:a-fA-F]+) port (?P<port>\d+)"
+        ),
+        "success",
+    ),
+]
